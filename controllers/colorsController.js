de267188e7162966b898d2665b76de6b64f01fa2
gerardjr42 <ggarciajr12@gmail.com>
2024-07-24 00:34:50 -0400

    update and delete complete

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -3,7 +3,7 @@ const express = require("express");
 const colors = express.Router();
 
 //Require our query for colors
-const { getAllColors, getColor, createColor } = require("../queries/color");
+const { getAllColors, getColor, createColor, deleteColor, updateColor } = require("../queries/color");
 
 //Requre our validations for colors
 const { checkName, checkBoolean } = require("../validations/checkColors.js");
@@ -33,6 +33,24 @@ colors.get("/:id", async (req, res) => {
 colors.post("/", checkName, checkBoolean, async (req, res) => {
   const color = await createColor(req.body);
   res.json(color);
+});
+
+//Delete
+colors.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedColor = await deleteColor(id)
+  if (deletedColor.id) {
+    res.status(200).json(deletedColor);
+  } else {
+    res.status(400).json("Color not found");
+  }
+});
+
+//Update
+colors.put("/:id", checkName, checkBoolean, async (req, res) => {
+  const { id } = req.params;
+  const updatedColor = await updateColor(id, req.body);
+  res.status(200).json(updatedColor);
 });
 
 module.exports = colors;
--- queries/color.js
+++ queries/color.js
@@ -33,8 +33,36 @@ const createColor = async (color) => {
   }
 };
 
+//Query to delete color
+const deleteColor = async (id) => {
+  try {
+    const deleteColor = await db.one(
+      "DELETE FROM colors WHERE id = $1 RETURNING *",
+      id
+    );
+    return deleteColor;
+  } catch (error) {
+    return error;
+  }
+};
+
+//Query to update color
+const updateColor = async (id, color) => {
+  try {
+    const updatedColor = await db.one(
+      "UPDATE colors SET name=$1, is_favorite=$2 where id=$3 RETURNING *",
+      [color.name, color.is_favorite, id]
+    );
+    return updatedColor;
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = { 
   getAllColors,
   createColor,
   getColor,
+  deleteColor,
+  updateColor,
 };
